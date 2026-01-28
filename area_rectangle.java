43057909a7147a1636dc6254c9a8ffd35207439d
shambhavi-suresh <shambhavisuresh1808@gmail.com>
2026-01-28 16:00:34 +0530

    Add files via upload

---
+++ area_rectangle.java
@@ -0,0 +1,14 @@
+package day_02;
+import java.util.Scanner;
+public class area_rectangle {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the lenghth");
+		int l=sc.nextInt();
+		System.out.println("enter the breadth");
+		int b=sc.nextInt();
+		int a=l*b;
+		System.out.println("a="+a);
+	}1
+
+}

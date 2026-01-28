bb241fe053c7073fb168acf78a2c6af198da2501
shambhavi-suresh <shambhavisuresh1808@gmail.com>
2026-01-28 16:01:11 +0530

    Add files via upload

---
+++ multi_var_assign.java
@@ -0,0 +1,11 @@
+package day_02;
+
+public class multi_var_assign {
+	public static void main(String[] args) {
+		int a=10,b=20,c=30;
+		System.out.println("a="+a);
+		System.out.println("b="+b);	
+		System.out.println("c="+c);
+	}
+
+}
